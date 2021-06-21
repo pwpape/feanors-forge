8b67dd868a2ac461c57b3daef6eb07ca3503457b
Patrick Pape <asiOtus@localhost.localdomain>
2021-06-21 14:23:00 -0500

    added data function in app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,5 +1,10 @@
 const app = Vue.createApp({
-    //
+    data() {
+        return {
+            title: 'The Silmarillion',
+            author: 'J.R.R. Tolkien'
+        }
+    }
 })
 
 app.mount('#app')
