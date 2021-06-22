f254171ffbf0eee16e524d2c1f2c3266779ff81b
Patrick Pape <asiOtus@localhost.localdomain>
2021-06-22 08:56:45 -0500

    added buttons

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,7 +2,20 @@ const app = Vue.createApp({
     data() {
         return {
             title: 'The Silmarillion',
-            author: 'J.R.R. Tolkien'
+            author: 'J.R.R. Tolkien',
+            rings: 1,
+            race: 'Men'
+        }
+    },
+    methods: {
+        increaseRings() {
+            this.rings++
+        },
+        decreaseRings() {
+            this.rings--
+        },
+        changeRace() {
+            this.race = 'Elves'
         }
     }
 })
